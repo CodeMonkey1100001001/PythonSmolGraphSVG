1b5eca5d1a5ab36837fc286f1dab3636caf646fd
CodeMonkey1100001001 <conf2015@mymail.themib.com>
2023-08-29 07:31:35 -0500

    pep cleanup

--- pythonfilter.py
+++ pythonfilter.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 # example:
-#  cat /tmp/misc/input.svg | python3 ./pythonfilter.py >/tmp/misc/output.svg
+# cat /tmp/misc/input.svg | python3 ./pythonfilter.py >/tmp/misc/output.svg
 
 import sys
 
 for line in sys.stdin:
     # line = line.replace('','')
-    line = line.replace('stroke="#000000','stroke="#ffffff')
+    line = line.replace('stroke="#000000', 'stroke="#ffffff')
     line = line.replace('fill="#000000"', 'fill="#ffffff"')
-    line = line.replace('stroke = "#000"','stroke = "#ffffff"')
-    line = line.replace('stroke="#000"','stroke="#ffffff"')
+    line = line.replace('stroke = "#000"', 'stroke = "#ffffff"')
+    line = line.replace('stroke="#000"', 'stroke="#ffffff"')
     line = line.replace('fill="#000"', 'fill="#ffffff"')
-    print(line,end='')
-
-
+    print(line, end='')
